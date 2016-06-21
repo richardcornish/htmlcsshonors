8ccdc86a8c771892da3a3261929b3f9fae1e49b3
Richard Cornish <richardcornish@gmail.com>
2016-06-21 16:55:30 -0500

    Added schedule today highlight

--- htmlcsshonors/static/js/script.js
+++ htmlcsshonors/static/js/script.js
@@ -1,1 +1,37 @@
+// Honors object
+var Honors = (function ($) {
+     'use strict';
+     var highlightToday = function () {
 
+         // Today's date
+         var now = moment();
+         var year = moment().year();
+         var month = moment().month();
+         var date = moment().date();
+         var today = moment().year(year).month(month).date(date);
+
+         // Class dates
+         var date_list = [];
+         $('tr.row-date').each(function () {
+             var item = $(this).attr('class').split(/\s+/);
+             date_list = date_list.concat(item);
+         });
+
+         // Highlights
+         $.each(date_list, function (index, item) {
+             if (moment(item).isSame(today, 'day')) {
+                 $('tr.' + item).addClass('golden');
+             }
+         });
+
+     }
+     return {
+        'highlightToday': highlightToday
+     }
+ })(jQuery);
+
+// Document ready
+jQuery(function () {
+    'use strict';
+    Honors.highlightToday();
+});
