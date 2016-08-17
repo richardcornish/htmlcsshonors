3f74cd3cac38995c9b54d883366a816aa8643e37
Richard Cornish <richardcornish@gmail.com>
2016-08-17 00:29:45 -0500

    Made relevant links open in new windows

--- htmlcsshonors/static/js/script.js
+++ htmlcsshonors/static/js/script.js
@@ -1,41 +1,55 @@
-// Honors object
-var Honors = (function ($) {
+var Honors = window.Honors || {};
+
+Honors = (function ($) {
 
      'use strict';
 
-     var highlightToday = function () {
-
-         // Today's date
-         var now = moment();
-         var year = moment().year();
-         var month = moment().month();
-         var date = moment().date();
-         var today = moment().year(year).month(month).date(date);
-
-         // Class dates
-         var date_list = [];
-         $('tr.row-date').each(function () {
-             var item = $(this).attr('class').split(/\s+/);
-             date_list = date_list.concat(item);
-         });
-
-         // Highlights
-         $.each(date_list, function (index, item) {
-             if (moment(item).isSame(today, 'day')) {
-                 $('tr.' + item).addClass('golden');
-             }
-         });
-
-     };
-
      return {
-        'highlightToday': highlightToday
+
+        openLinkInNewWindow: function () {
+            $('a.js-external, a[rel="external"]').on('click', function (event) {
+                var href;
+                href = $(this).attr('href');
+                window.open(href);
+                event.preventDefault();
+            });
+        },
+
+        highlightToday: function () {
+
+            // Today's date
+            var now = moment();
+            var year = moment().year();
+            var month = moment().month();
+            var date = moment().date();
+            var today = moment().year(year).month(month).date(date);
+
+            // Class dates
+            var date_list = [];
+            $('tr.row-date').each(function () {
+                var item = $(this).attr('class').split(/\s+/);
+                date_list = date_list.concat(item);
+            });
+
+            // Highlights
+            $.each(date_list, function (index, item) {
+                if (moment(item).isSame(today, 'day')) {
+                    $('tr.' + item).addClass('golden');
+                }
+            });
+
+        },
+
+        ready: function () {
+            this.openLinkInNewWindow();
+            this.highlightToday();
+        }
+
      };
 
- })(jQuery);
+ }(jQuery));
 
-// Document ready
 jQuery(document).ready(function () {
     'use strict';
-    Honors.highlightToday();
+    Honors.ready();
 });
